a81307efd2805a8885c7b2bc23d723b38697db3b
Manu <manusasson@gmail.com>
2023-11-28 21:48:15 -0300

    Arreglos del after

--- Entregables/src/routes/carts.routers.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router()
-
-router.get('/carts',(req,res) => {
-//cuerpo
-})
-
-export default router
--- Entregables/src/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router()
-
-router.get('/productos',(req,res) => {
-//cuerpo
-})
-
-export default router
