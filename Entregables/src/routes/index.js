c772dd6e0708dbce258dbd7710f255102e48dafb
Manuel Sasson <msasson@pvci.com.uy>
2024-03-11 17:24:27 -0300

    Arreglo main

--- Entregables/src/routes/index.js
+++ Entregables/src/routes/index.js
@@ -3,6 +3,7 @@ const userRouter = require('./users.router.js')
 const viewsRouter = require('./views.router.js')
 const sessionsRouter = require('./sessions.router.js')
 const prodcutsRouter = require('./products.router.js')
+const cartsRouter = requier('./carts.router.js')
 const { uploader } = require('../utils/upoloader.js')
 const mockingProductsRouter = require('./mockingproducts.router.js'); 
 
@@ -14,10 +15,10 @@ router.use('/', viewsRouter)
 router.use('/api/sessions', sessionsRouter)
 router.use('/api/users', userRouter)
 router.use('/api/products', prodcutsRouter)
-router.use('/api/carts', ()=>{})
+router.use('/api/carts', cartsRouter)
 router.post('/uploader', uploader.single('myFile'), (req, res)=>{
 
-    res.send('Imagen subida')
+    res.send('Imagen subida!')
 })
 router.use('/api/mockingproducts', mockingProductsRouter); // Agregar el nuevo enrutador
 
